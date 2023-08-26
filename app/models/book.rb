30635148917d448d45b21c0ee5280ecf353333c6
Alex Kuhn <a.kun@dev.igooods.ru>
2023-08-26 20:38:40 +0300

    add book validations, add turbo callbacks to the book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  after_create_commit { broadcast_prepend_to :books }
+  after_update_commit { broadcast_replace_to "book_#{id}" }
+
+  validates :title, :description, presence: true
 end
